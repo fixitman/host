8d5845ac6e3097d039b50754c34091bce1c12edc
Mike Collins <fixitman.mike@outlook.com>
2025-03-11 16:35:22 -0400

    added listbox for reminder list

--- ui/MainView.cs
+++ ui/MainView.cs
@@ -1,36 +1,27 @@
 
-using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Hosting;
 using Reminder_WPF.Models;
 using Reminder_WPF.Services;
 using Reminder_WPF.Utilities;
-using Serilog;
 using Terminal.Gui;
 
 namespace host.ui {
-
 
     public partial class MainView 
     {
         private APIReminderRepo? _repo;
         private IConfiguration _config;
-        private SettingsManager<UserSettings> _setMgr;
         private ILogger<MainView> _log {get;}
-        private IHost _host;
 
         public MainView(
-            SettingsManager<UserSettings> setMgr,
             IConfiguration configuration,
-            IHost host,
             ILogger<MainView> logger
         ){
-            _setMgr = setMgr;
             _log = logger;
             _config = configuration;
-            _host = host;
             InitializeComponent();
+            listView.SetSource(new List<string>());
             this.Loaded += Run;
         }
 
@@ -41,23 +32,38 @@ namespace host.ui {
             token = response.token;
             _log.LogDebug("Token = {token}  expires {exp}", response.token, response.expiration);
             _repo = new APIReminderRepo(_config.GetValue<string>("Server:URL","UNDEFINED"),token,_config.GetValue<int>("Server:Port"));
+            var r = await _repo.GetRemindersAsync();
+            if(r.IsFailure){
+                MessageBox.ErrorQuery("Error",$"Could not get reminders: {r.Error}","Ok");
+            }else if( r.IsEmpty){
+                MessageBox.Query($"","No reminders yet.","Ok");
+            }else{
+                MessageBox.Query("Success",$"{r.Value.Count} reminders retrieved.", "Ok");
+                var texts = r.Value.Select(e => e.ReminderText).ToList();
+                var widest = texts.Max(t => t.Length);
+                listView.Height = texts.Count;
+                listView.Width = widest + 2;
+                listView.SetSource(texts);
+            }
+            
         }
 
         private async Task<LoginResponse> Login(){
             var _user = "";
             var _pass = "";
             var error = "";
-            Result<LoginResponse?> response;
+            Result<LoginResponse?> result;
             do
             {
                 var dlg = new LoginDialog(_user, _pass, error);               
                 Application.Run(dlg);
-                response = await APIReminderRepo.GetToken(dlg.User, dlg.Password, _config.GetValue<string>("Server:URL", "NotDefined"), _config.GetValue<int>("Server:Port"));
-                if (!response.IsFailureOrEmpty)
+                result = await APIReminderRepo
+                    .GetToken(dlg.User, dlg.Password, _config.GetValue<string>("Server:URL", "NotDefined"), _config.GetValue<int>("Server:Port"));
+                if (!result.IsFailureOrEmpty)
                 {
-                    return response.Value!;
+                    return result.Value!;
                 }
-                error = $"{response.Error}";
+                error = $"{result.Error}";
                 _user = dlg.User;
                 _pass = dlg.Password;
             } while (true);            
